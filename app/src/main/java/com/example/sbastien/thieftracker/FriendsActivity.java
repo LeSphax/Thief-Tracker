1f6c36d47fc9b728228b5a5608c3c02f027035a5
yavo <yannyavo@gmail.Com>
2015-11-05 17:20:42 +0100

    Add Friend list windows

--- app/src/main/java/com/example/sbastien/thieftracker/FriendsActivity.java
+++ app/src/main/java/com/example/sbastien/thieftracker/FriendsActivity.java
@@ -6,8 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.sbastien.thieftracker.model.Friends;
+import com.example.sbastien.thieftracker.util.FriendListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FriendsActivity extends AppCompatActivity {
+
+    FriendListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,15 @@ public class FriendsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_friends);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        List<Friends> numberList = new ArrayList<>();
+        Friends one = new Friends("pops","03585647858");
+        numberList.add(one);
+
+        adapter = new FriendListAdapter(this, R.layout.item_friend_layout, numberList);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +43,11 @@ public class FriendsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void removeFriend(View view) {
+        Friends item = (Friends) view.getTag();
+        adapter.remove(item);
     }
 
 }
