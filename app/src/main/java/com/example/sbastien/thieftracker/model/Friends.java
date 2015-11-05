62e60f46bb4193a5c600782b5035e0cbb7a72fc9
yavo <yannyavo@gmail.Com>
2015-11-05 17:37:44 +0100

    Add Friend list windows

---
+++ app/src/main/java/com/example/sbastien/thieftracker/model/Friends.java
@@ -0,0 +1,29 @@
+package com.example.sbastien.thieftracker.model;
+
+
+public class Friends {
+
+    String name;
+    String phoneNumber;
+
+    public Friends(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
